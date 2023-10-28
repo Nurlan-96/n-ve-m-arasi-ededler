f88df15ce3c3791b1090d3b31dc324a2daaf40af
Nurlan <nurlanea@code.edu.az>
2023-10-29 01:17:46 +0400

    first

---
+++ Methods/Program.cs
@@ -0,0 +1,32 @@
+﻿//int counter = 0;
+
+//int numbers(int n, int m)
+//{
+//    while (n < m)
+//    {
+//        if (n % 2 != 0)
+//        {
+//            counter++;
+//        }
+//     n++;
+//    }
+//    return counter;
+//}
+//Console.WriteLine(numbers(1, 10));
+
+
+int sum = 0;
+int SumNumbers(int n, int m)
+{
+    while (n < m)
+    {
+        if (n % 2 != 0)
+        {
+            sum += n;
+        }
+        n++;
+    }
+    return sum;
+}
+       
+Console.WriteLine(SumNumbers(1,10));
